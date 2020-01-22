afec8edef666353dd536148d8315e2ba46a6786d
Nilsen Reyes <nreyes1202@yahoo.com>
2020-01-21 23:29:57 -0700

    added click event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,19 @@
-$(document).ready(function() ){
+$(document).ready(function() {
+
+    var minNumber = 19;
+    var maxNumber = 120;
+    var wins = 0;
+    var losses = 0;
+    var totalScore = [];
+
+    //var randomnumber = Math.floor(Math.random() );
+    //var randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+    $( "#crystal1" ).on("click", function() {
+        totalScore += 1;
+        alert( "Clicked!" );
+      });
+      
     
 }
+)
+
